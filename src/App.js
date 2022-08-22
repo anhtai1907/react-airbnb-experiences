638716bf664bdf2cb894f280371c97b07a526ca9
Tai Anh <anhtai.1907@gmail.com>
2022-08-22 21:02:28 +0700

    Import data from data.js and spread object as props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-        <Hero/>
-    </div>
-  );
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
+    return (
+        <div className="app">
+            <Navbar/>
+            <Hero/>
+            <section className={"card-list"}>
+                {cards}
+            </section>
+        </div>
+    );
 }
 
 export default App;
